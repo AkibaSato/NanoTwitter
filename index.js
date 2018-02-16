8e76a104d3a781e8e45a302a14028782199521c0
Akiba Sato <akibasato1992@gmail.com>
2018-02-15 21:16:32 -0500

    Changed it back.

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// var http = require('http');
-//
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('Hello World!');
-// }).listen(8080);
-
-
-const express = require('express')
-// const config = require('./config/config')
-
-const app = express()
-// config(app)
-
-const port = 3000
-
-app.listen(process.env.PORT || port)
-
 var http = require('http');
 
 http.createServer(function (req, res) {
