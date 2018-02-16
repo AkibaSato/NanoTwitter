0d8678e85aa164faa67a229d57ae395e273e4420
Akiba Sato <akibasato1992@gmail.com>
2018-02-15 21:15:13 -0500

    Added changes for Heroku.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// var http = require('http');
+//
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello World!');
+// }).listen(8080);
+
+
+const express = require('express')
+// const config = require('./config/config')
+
+const app = express()
+// config(app)
+
+const port = 3000
+
+app.listen(process.env.PORT || port)
+
 var http = require('http');
 
 http.createServer(function (req, res) {
