9c7327bfeb959868afccbf2bae3187642f31a302
Akiba Sato <akibasato1992@gmail.com>
2018-02-27 19:09:45 -0500

    Trying to make my MongoDB work.

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,19 @@
+const mongoose = require('mongoose');
+const Tweet = mongoose.model('Tweet');
+const User = mongoose.model('User');
+
 module.exports.index = function (req, res, next) {
-  res.render('index');
+  let followingCount = req.user.followees.length;
+  let followerCount = req.user.followers.length;
+
+  res.render('index', {
+        title: 'List of Tweets',
+        tweets: tweets,
+        page: page + 1,
+        tweetCount: tweetCount,
+        pagination: pagination,
+        followerCount: followerCount,
+        followingCount: followingCount,
+        pages: Math.ceil(pageViews / perPage),
+      });
 };
