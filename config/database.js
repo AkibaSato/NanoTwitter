b604553eef03f7c0640ae40b7778f197d19422e0
Kevin <kwang30@gmail.com>
2018-02-27 19:46:56 -0500

    add db config

--- config/database.js
+++ config/database.js
@@ -1,3 +1,11 @@
-module.exports = {
-  'localURL': "mongodb://localhost/nt-dev",
+
+module.exports = function(app, mongoose){
+  var env=require('dotenv').config();
+  console.log(process.env.LOCAL_URL)
+  if (app.get('env') === 'development') {
+    mongoose.connect(process.env.LOCAL_URL);
+  } else {
+    mongoose.connect(process.env.HEROKU_URL);
+  }
+
 }
