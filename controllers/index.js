99f7fd32a296b622527a11d6df8fde8106680530
Akiba Sato <akibasato1992@gmail.com>
2018-02-27 20:21:48 -0500

    Started working on views.

--- controllers/index.js
+++ controllers/index.js
@@ -3,17 +3,9 @@ const Tweet = mongoose.model('Tweet');
 const User = mongoose.model('User');
 
 module.exports.index = function (req, res, next) {
-  let followingCount = req.user.followees.length;
-  let followerCount = req.user.followers.length;
+
 
   res.render('index', {
         title: 'List of Tweets',
-        tweets: tweets,
-        page: page + 1,
-        tweetCount: tweetCount,
-        pagination: pagination,
-        followerCount: followerCount,
-        followingCount: followingCount,
-        pages: Math.ceil(pageViews / perPage),
       });
 };
