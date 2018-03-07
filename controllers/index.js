612525f3827703c16cbb0d3b127996a082741790
Akiba Sato <akibasato1992@gmail.com>
2018-03-07 17:50:50 -0500

    Edited index page.

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,7 @@
 const User = require('../models').User;
 
 module.exports.index = (req, res, next) => {
-  console.log(req.user.username);
   res.render('index', {
     req: req
-    // title: 'List of Tweets',
   });
 };
