327915fe399658a7929a4aaf9d6e6bec00a1c014
mgkbsh <mkobashi@brandeis.edu>
2018-03-12 13:32:35 -0400

    Added missing routes for retweets

--- routes/tweets.js
+++ routes/tweets.js
@@ -6,5 +6,7 @@ router.post('/new', tweets.tweet);
 router.get('/:id', tweets.getTweet);
 router.get('/:id/likes', tweets.getLikes);
 router.post('/:id/like', tweets.like);
+router.get('/:id/likes', tweets.getRetweets);
+router.post('/:id/like', tweets.retweets);
 
 module.exports = router;
