4caa94b666b204bd8dc8acd019c06392afd0bbe3
mgkbsh <mkobashi@brandeis.edu>
2018-03-22 01:03:41 -0400

    Redirect to index if tries to perform logged-in only actions

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -4,5 +4,5 @@ module.exports.isLoggedIn = function(req, res, next) {
         return next();
 
     // If they aren't redirect them to the home page.
-    res.redirect('/login');
+    res.redirect('/');
 }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var users = require('../controllers/users');
 var passport = require('passport');
 var router = express.Router();
+var isLoggedIn = require('../middleware/isLoggedIn')
 
 router.get('/register', users.getSignup);
 router.get('/:id', users.getUser);
@@ -9,8 +10,8 @@ router.get('/:id/tweets', users.getTweets);
 router.get('/:id/followees', users.getFollowees);
 router.get('/:id/followers', users.getFollowers);
 router.get('/:id/followee_tweets', users.getFolloweeTweets);
-router.post('/:id/follow', users.follow);
-router.delete('/:id/follow', users.unfollow);
+router.post('/:id/follow', isLoggedIn, users.follow);
+router.delete('/:id/follow', isLoggedIn, users.unfollow);
 
 router.post('/register', passport.authenticate('local-signup', {
   successRedirect: '/',
