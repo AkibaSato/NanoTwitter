4d7b8cfd0089119a54567fd0b8d14c1d31a5b5db
mgkbsh <mkobashi@brandeis.edu>
2018-04-06 21:42:11 -0400

    Use search service

--- controllers/search.js
+++ controllers/search.js
@@ -1,124 +1,19 @@
-var models = require('../models')
+var env = process.env.NODE_ENV || 'development';
+var config = require('../config/config.json')[env]
+var searchServiceURL = config.search_service
 
-// Example return JSON:
-// [{
-//  "content": "hey",
-//  "createdAt": "2018-03-09T04:30:08.385Z",
-//  "user": {
-//   "username": "bob_builder"
-//  }
-// },
-// {
-//  "content": "hey bob_builder",
-//  "createdAt": "2018-03-09T04:30:08.385Z",
-//  "user": {
-//   "username":"dora_explorer"
-//  }
-// }]
-module.exports.search = (req, res, next) => {
-  var term = req.query.search;
-  var searchPromise;
-  if (term.startsWith('#')) {
-    searchPromise = searchHashtag(term.substring(1));
-  } else if (term.startsWith('@')) {
-    searchPromise = searchMention(term.substring(1));
-  } else {
-    searchPromise = searchWord(term);
-  };
-  searchPromise
-  .then(tweets => {
-    res.render("search", {tweets: JSON.parse(JSON.stringify(tweets)), user:req.user});
-  }).catch(err => {
-    res.status(404).send(err);
-  });
-};
+var axios = require('axios')
 
-// Return promise to search for term through Hashtag table.
-// TODO: Crazy four-table join.
-function searchHashtag(term) {
-  return models.HashtagTweet.findAll({
-    include: [
-      {
-        model: models.Hashtag,
-        as: 'hashtag',
-        attributes: ['content'],
-        where: { 'content': term }
-      },
-      {
-        model: models.Tweet,
-        as: 'tweet',
-        attributes: ['content', 'createdAt'],
-        include: [{
-          model: models.User,
-          as: 'user',
-          attributes: ['username']
-        }]
-      }
-    ],
-    attributes: ['createdAt']
-  }).then(tweets => {
-    return tweets.map(x => flattenHashtagJson(x));
-  });
-};
+module.exports.search = async (req, res) => {
+  try {
 
-// JSONify a single row in the joined table from searchHashtag().
-function flattenHashtagJson(x) {
-  return {
-    createdAt: x.tweet.createdAt,
-    content: x.tweet.content,
-    user: x.user
-  };
-}
+    var tweets = await axios.get(searchServiceURL + '/search', {
+      data: { term: req.query.search }
+    });
 
-// Return promise to search for term through Mention table.
-function searchMention(term) {
-  return models.Mention.findAll({
-    include: [
-      {
-        model: models.User,
-        as: 'user',
-        where: { 'username': term },
-        attributes: ['createdAt']
-      },
-      {
-        model: models.Tweet,
-        as: 'tweet',
-        attributes: ['content', 'createdAt'],
-        include: [{
-          model: models.User,
-          as: 'user',
-          attributes: ['username']
-        }]
-      }
-    ],
-    attributes: ['createdAt']
-  }).then(tweets => {
-    return tweets.map(x => flattenMentionJson(x));
-  });
-};
+    res.render("search", { tweets: tweets.data, me: req.user, original: false });
 
-// JSONify a single row in the joined table from searchMention().
-function flattenMentionJson(x) {
-  return {
-    createdAt: x.tweet.createdAt,
-    content: x.tweet.content,
-    user: x.tweet.user
-  };
-}
-
-// Return promise to search for term through Tweet table.
-function searchWord(term) {
-  return models.Tweet.findAll({
-    where: {
-      content: {
-        $like: '%' + term + '%'
-      }
-    },
-    include: [{
-      model: models.User,
-      as: 'user',
-      attributes: ['username']
-    }],
-    attributes: ['content', 'createdAt']
-  });
+  } catch (err) {
+    res.status(404).send(err)
+  }
 };
