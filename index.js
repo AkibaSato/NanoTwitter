35f73c3c93e1fe0e6d5abe6cd6c73ee6e1231734
Kevin <kwang30@gmail.com>
2018-04-16 00:54:58 -0400

    added cluster cpu module"

--- index.js
+++ index.js
@@ -3,6 +3,27 @@
   const port = process.env.PORT || 3000;
   const path    = require('path');
   const redis = require('redis');
+  var cluster = require('cluster'); //no need to download anything
+  var os = require('os'); //no need to download anything
+
+  if(cluster.isMaster) {
+     var numWorkers = os.cpus().length;
+     console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+     for(var i = 0; i < numWorkers; i++) {
+         cluster.fork();
+     }
+
+     cluster.on('online', function(worker) {
+         console.log('Worker ' + worker.process.pid + ' is online');
+     });
+
+     cluster.on('exit', function(worker, code, signal) {
+         console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+         console.log('Starting a new worker');
+         cluster.fork();
+     });
+   } else {
 
   /* ===========PARSER=========== */
   const bodyParser = require('body-parser');
@@ -88,3 +109,4 @@
 
   app.listen(port);
   exports = module.exports = app;
+}
