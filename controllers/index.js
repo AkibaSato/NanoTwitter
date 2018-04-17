45f97f57c1078508ac125a888d70180701770e7a
mgkbsh <mkobashi@brandeis.edu>
2018-04-17 19:39:28 -0400

    Full controllers w/o caching

--- controllers/index.js
+++ controllers/index.js
@@ -7,43 +7,31 @@ var axios = require('axios')
 
 module.exports.index = async (req, res) => {
   try {
-    // var timeline
-    // var user
-    // if (req.user) {
-    //   var getUser = axios.get(userServiceURL + '/user', {
-    //     data: { id: req.user.id }
-    //   });
-    //
-    //   // If you are logged in, you see tweets from the people you follow.
-    //   var getTimeline = axios.get(tweetServiceURL + '/timeline/followees', {
-    //     data: { id: req.user.id }
-    //   });
-    //
-    //   var [userData, timelineData] = await axios.all([getUser, getTimeline]);
-    //
-    //   req.user = userData.data
-    //   timeline = timelineData
-    //
-    // } else {
-    //   // If you are not, you see the most recent tweets from randos.
-    //   timeline = await axios.get(tweetServiceURL + '/timeline/global', {});
-    // }
-    //
-    await axios.get(tweetServiceURL + '/timeline/global', {});
+    var timeline
+    var user
+    if (req.user) {
+      var getUser = axios.get(userServiceURL + '/user', {
+        data: { id: req.user.id }
+      });
+
+      // If you are logged in, you see tweets from the people you follow.
+      var getTimeline = axios.get(tweetServiceURL + '/timeline/followees', {
+        data: { id: req.user.id }
+      });
+
+      var [userData, timelineData] = await axios.all([getUser, getTimeline]);
+
+      req.user = userData.data
+      timeline = timelineData
+
+    } else {
+      // If you are not, you see the most recent tweets from randos.
+       timeline = await axios.get(tweetServiceURL + '/timeline/global', {});
+    }
+
     res.render('index', {
-      me: req.user, user: req.user, tweets: [
-          {
-              "id": 18,
-              "content": "hi",
-              "createdAt": "2018-04-06T03:10:04.011Z",
-              "user": {
-                  "fullName": "yoyo yoyo",
-                  "id": 1,
-                  "username": "yoyo"
-              }
-          }
-      ], API_TOKEN: req.API_TOKEN
-    });
+      me: req.user, user: req.user, tweets: timeline.data, API_TOKEN: req.API_TOKEN
+    })
     // res.json({});
   } catch (err) {
     res.status(404).send(err)
