b1a5c1e931c226dade1f02271ae2528b383b760f
mgkbsh <mkobashi@brandeis.edu>
2018-04-24 02:19:43 -0400

    Loaderio params

--- middleware/populateUser.js
+++ middleware/populateUser.js
@@ -65,8 +65,8 @@ module.exports = async (req, res, next) => {
       if (req.body.public == "true") {
         delete req.user;
       } else {
-        req.user = { id: req.query.id, fname: loaderio,
-          lname: loaderio, username: laoderio };
+        req.user = { id: req.body.id, fname: 'loaderio',
+          lname: 'loaderio', username: 'loaderio' };
       }
       return next();
     }
