393a20754cb5b5d4f6f5f7c6af392e63eeb69dfa
mgkbsh <mkobashi@brandeis.edu>
2018-04-24 05:05:05 -0400

    Changed to cloud redis

--- controllers/index.js
+++ controllers/index.js
@@ -4,7 +4,7 @@ var tweetServiceURL = config.tweet_service
 var userServiceURL = config.user_service
 // var async=require('async')
 var axios = require('axios')
-// var redis = require("redis")
+var redis = require('../config/redis')
 // var REDIS_PORT = process.env.REDISCLOUD_URL || process.env.REDIS_PORT;
 // var client = redis.createClient(REDIS_PORT);
 
